97f85ea3450ce3917753fcb4e3fca66085c8757d
Catalina Morera <cmorera@rms.com>
2023-06-12 12:16:20 -0700

    update proeject libraries

--- src/Owners/Views/List/PetListData.tsx
+++ src/Owners/Views/List/PetListData.tsx
@@ -1,15 +1,11 @@
-import Carousel  from "../../../Utils/Carousel/Carousel";
+
 
 export default function PetListData(){
 
     return (
         <>
         
-        <div className="form-container"> 
-            <h1>PET REGISTER</h1> 
-            
-            <Carousel/>
-            </div>
+       
         </>
 
     )
