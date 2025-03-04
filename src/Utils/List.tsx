e89a478820f2e8e699dd79a8e710d177c819caf3
Cαtαliηα Morera Granados <moreracata@gmail.com>
2025-03-03 18:59:50 -0600

    fix file name

--- src/Utils/List.tsx
+++ src/Utils/List.tsx
@@ -1,36 +1,36 @@
-import ListOptionData  from "./ListOptionData";
-import './css/list.css';
-import Paginator  from './Paginator';
+import ListOptionData from "./ListOptionData";
+import './css/List.css';
+import Paginator from './Paginator';
 
-export default function List(props: ListProps){
-   
+export default function List(props: ListProps) {
+
     return (
         <>
-                <div className="row ">   
-                    <div className="col-lg-12">
-                        <div className="main-box clearfix">
-                            <div className="table-responsive">
-                                <table className="table user-list">
+            <div className="row ">
+                <div className="col-lg-12">
+                    <div className="main-box clearfix">
+                        <div className="table-responsive">
+                            <table className="table user-list">
                                 <thead>
                                     <tr>
-                                    <th><span>Name</span></th>
-                                    <th><span>Date of Birth</span></th>
-                                    <th className="text-center"><span>Specie</span></th>
-                                    <th><span>Owner</span></th>
-                                    <th><span>Options</span></th>
-                                    
+                                        <th><span>Name</span></th>
+                                        <th><span>Date of Birth</span></th>
+                                        <th className="text-center"><span>Specie</span></th>
+                                        <th><span>Owner</span></th>
+                                        <th><span>Options</span></th>
+
                                     </tr>
                                 </thead>
                                 <tbody>
-                                {props.options.map(option =>
-                                <ListOptionData listObject={option} key={option.id} />)}
+                                    {props.options.map(option =>
+                                        <ListOptionData listObject={option} key={option.id} />)}
                                 </tbody>
-                                </table>
-                            </div>
-                            <Paginator numberOfPages={5} currentPage={3} />
+                            </table>
                         </div>
+                        <Paginator numberOfPages={5} currentPage={3} />
                     </div>
-                </div>  
+                </div>
+            </div>
         </>
     )
 
@@ -39,5 +39,5 @@ export default function List(props: ListProps){
 interface ListProps {
     headerOption: string[];
     options: any[]
- 
+
 }
