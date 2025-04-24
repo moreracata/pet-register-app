053e9d6627a240955e7759164eeaa6e76586c212
CATALINA\morer <moreracata@gmail.com>
2025-04-23 18:32:39 -0600

    no message

--- src/Utils/Carousel/Carousel.tsx
+++ src/Utils/Carousel/Carousel.tsx
@@ -2,25 +2,11 @@ import CarouselOption from "./CarouselOption";
 import './Style/Carousel.css';
 import { useEffect, useState } from 'react';
 import { CarouselOptionDTO } from "./Models/CarouselOptionDTO"
+import options from "./Data/CarouselOptions"
 
 export default function Carousel(props: carouselProps) {
 
-    const options: CarouselOptionDTO[] = [
-        { id: 0, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193375.png', tag: 'OPCION0' },
-        { id: 1, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193256.png', tag: 'OPCION1' },
-        { id: 2, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193260.png', tag: 'OPCION2' },
-        { id: 3, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193312.png', tag: 'OPCION3' },
-        { id: 4, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193308.png', tag: 'OPCION4' },
-        { id: 5, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193288.png', tag: 'OPCION5' },
-        { id: 6, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193354.png', tag: 'OPCION6' },
-        { id: 7, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193310.png', tag: 'OPCION7' },
-        { id: 8, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193251.png', tag: 'OPCION8' },
-        { id: 9, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193243.png', tag: 'OPCION9' },
-        { id: 10, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193285.png', tag: 'OPCION10' },
-        { id: 11, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193351.png', tag: 'OPCION11' },
-        { id: 12, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193347.png', tag: 'OPCION12' },
 
-    ]
     const max: number = options.length;
 
     const [current, setCurrent] = useState(
@@ -44,7 +30,6 @@ export default function Carousel(props: carouselProps) {
         let newCenter = options[(centerId - 1 + max) % max];
         var newCurrent = [newLeft, newCenter, newRight]
         setCurrent(newCurrent)
-
     }
 
     const moveToRight = () => {
