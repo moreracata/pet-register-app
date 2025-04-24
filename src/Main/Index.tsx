63f354e7e6b5332726be835f7799b4feb8a43c75
CATALINA\morer <moreracata@gmail.com>
2025-04-23 18:24:05 -0600

    no message

--- src/Main/Index.tsx
+++ src/Main/Index.tsx
@@ -1,30 +1,30 @@
 
 import IndexOption from './IndexOption';
-import {IndexOptionDTO} from './Models/IndexOptionDTO';
+import { IndexOptionDTO } from './Models/IndexOptionDTO';
 
-export default function MainOptions(){
+export default function MainOptions() {
     const options: IndexOptionDTO[] = [
-        { id:1, image: 'https://cdn-icons-png.flaticon.com/512/2809/2809844.png', tag: 'PETS', url: '/Pet' }, 
-        { id:2, image: 'https://cdn-icons-png.flaticon.com/512/6381/6381452.png', tag: 'OWNERS' , url: '/Owner' }, 
-        { id:3, image: 'https://cdn-icons-png.flaticon.com/512/2259/2259486.png', tag: 'SPECIES', url: '/Specie'  },
-        { id:4, image: 'https://cdn-icons-png.flaticon.com/512/5389/5389413.png', tag: 'STORE', url: '/Store' }, 
-        { id:5, image: 'https://cdn-icons-png.flaticon.com/512/1292/1292651.png', tag: 'DOCTOR' , url: '/Doctor' }, 
-        { id:6, image: 'https://cdn-icons-png.flaticon.com/512/7965/7965936.png', tag: 'GROOMING', url: '/Gromming'  }
+        { id: 1, image: 'https://cdn-icons-png.flaticon.com/512/2809/2809844.png', tag: 'PETS', url: '/Pet' },
+        { id: 2, image: 'https://cdn-icons-png.flaticon.com/512/6381/6381452.png', tag: 'OWNERS', url: '/Owner' },
+        { id: 3, image: 'https://cdn-icons-png.flaticon.com/512/2259/2259486.png', tag: 'SPECIES', url: '/Specie' },
+        { id: 4, image: 'https://cdn-icons-png.flaticon.com/512/5389/5389413.png', tag: 'STORE', url: '/Store' },
+        { id: 5, image: 'https://cdn-icons-png.flaticon.com/512/1292/1292651.png', tag: 'DOCTOR', url: '/Doctor' },
+        { id: 6, image: 'https://cdn-icons-png.flaticon.com/512/7965/7965936.png', tag: 'GROOMING', url: '/Gromming' }
     ]
 
     return (
-        <> 
-             <h1>PET REGISTER OPTIONS</h1> 
-            <div className="row">   
+        <>
+            <h1>PET REGISTER OPTIONS</h1>
+            <div className="row">
                 <IndexOption option={options[0]} key='1' />
                 <IndexOption option={options[1]} key='2' />
                 <IndexOption option={options[2]} key='3' />
-            </div>    
-            <div className="row">   
+            </div>
+            <div className="row">
                 <IndexOption option={options[3]} key='4' />
                 <IndexOption option={options[4]} key='5' />
                 <IndexOption option={options[5]} key='6' />
-            </div>    
+            </div>
         </>
     )
 }
--- src/Utils/Carousel/Carousel.tsx
+++ src/Utils/Carousel/Carousel.tsx
@@ -1,67 +1,96 @@
-import CarouselOption  from "./CarouselOption";
+import CarouselOption from "./CarouselOption";
 import './Style/Carousel.css';
-
 import { useEffect, useState } from 'react';
-
 import { CarouselOptionDTO } from "./Models/CarouselOptionDTO"
 
-export default function Carousel(props: carouselProps){
+export default function Carousel(props: carouselProps) {
+
+    const options: CarouselOptionDTO[] = [
+        { id: 0, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193375.png', tag: 'OPCION0' },
+        { id: 1, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193256.png', tag: 'OPCION1' },
+        { id: 2, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193260.png', tag: 'OPCION2' },
+        { id: 3, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193312.png', tag: 'OPCION3' },
+        { id: 4, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193308.png', tag: 'OPCION4' },
+        { id: 5, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193288.png', tag: 'OPCION5' },
+        { id: 6, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193354.png', tag: 'OPCION6' },
+        { id: 7, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193310.png', tag: 'OPCION7' },
+        { id: 8, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193251.png', tag: 'OPCION8' },
+        { id: 9, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193243.png', tag: 'OPCION9' },
+        { id: 10, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193285.png', tag: 'OPCION10' },
+        { id: 11, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193351.png', tag: 'OPCION11' },
+        { id: 12, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193347.png', tag: 'OPCION12' },
+
+    ]
+    const max: number = options.length;
 
     const [current, setCurrent] = useState(
-            [ 
-                { id:1, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193256.png', tag: 'OPCION1'}, 
-                { id:2, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193260.png', tag: 'OPCION2' }, 
-                { id:3, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193312.png', tag: 'OPCION3' }
-            ]
+        [
+            { id: 1, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193256.png', tag: 'OPCION1' },
+            { id: 2, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193260.png', tag: 'OPCION2' },
+            { id: 3, image: 'https://cdn-icons-png.flaticon.com/512/4193/4193312.png', tag: 'OPCION3' }
+        ]
     )
-    
+
     const moveToLeft = () => {
-        let left = current[0].id
-        let right = current[2].id
-        
-            if( current[0].id != 0){
-           var newCurrent = [props.options[right-3],props.options[right-2],props.options[right-1]]
-           setCurrent(newCurrent)
-        }
+
+        //Current options
+        let leftId = current[0].id
+        let rightId = current[2].id
+        let centerId = current[1].id
+        //console.log(`${leftId}, ${centerId}, ${rightId}!`)
+
+        let newRight = options[(rightId - 1 + max) % max];
+        let newLeft = options[(leftId - 1 + max) % max];
+        let newCenter = options[(centerId - 1 + max) % max];
+        var newCurrent = [newLeft, newCenter, newRight]
+        setCurrent(newCurrent)
+
     }
 
     const moveToRight = () => {
-        
-        let left = current[0].id
-        let right = current[2].id
-       
-        if(right != props.options.length-1){
-            var newCurrent = [props.options[left+1],props.options[left+2],props.options[left+3]]
-           setCurrent(newCurrent)
-        }
+
+        //Current options
+        let leftId = current[0].id
+        let rightId = current[2].id
+        let centerId = current[1].id
+        //console.log(`${leftId}, ${centerId}, ${rightId}!`)
+
+        let newRight = options[(rightId + 1) % max];
+        let newLeft = options[(leftId + 1) % max];
+        let newCenter = options[(centerId + 1) % max];
+        var newCurrent = [newLeft, newCenter, newRight]
+        setCurrent(newCurrent)
+
     }
 
-    const onclickOption = () => {console.log("'../img/gato.png'a")}
-    
+
+
+    const onclickOption = () => { console.log("'../img/gato.png'a") }
+
     return (
-        <> 
-            <div className="custom-carousel">  
-                <div className="row"> 
-                    <div className="col-1"> 
+        <>
+            <div className="custom-carousel">
+                <div className="row">
+                    <div className="col-1">
                         <a className="prev" onClick={moveToLeft}>&#10094;</a>
                     </div>
-                    <div className="col-10"> 
-                            <div className="custom-carousel-options">  
-                                <CarouselOption custoClassName="left" onClick={onclickOption} option={current[0]} key={current[0].id} />
-                                <CarouselOption custoClassName="center" onClick={onclickOption} option={current[1]} key={current[1].id} />
-                                <CarouselOption custoClassName="right" onClick={onclickOption} option={current[2]} key={current[2].id} />
-                            </div>
+                    <div className="col-10">
+                        <div className="custom-carousel-options">
+                            <CarouselOption custoClassName="left" onClick={onclickOption} option={current[0]} key={current[0].id} />
+                            <CarouselOption custoClassName="center" onClick={onclickOption} option={current[1]} key={current[1].id} />
+                            <CarouselOption custoClassName="right" onClick={onclickOption} option={current[2]} key={current[2].id} />
                         </div>
-                    <div className="opt col-1"> 
+                    </div>
+                    <div className="opt col-1">
                         <a className="next" onClick={moveToRight}>&#10095;</a>
                     </div>
                 </div>
-            </div>   
+            </div>
         </>
-    )       
+    )
 }
- 
 
-interface carouselProps{
-    options: CarouselOptionDTO[];
+
+interface carouselProps {
+    options?: CarouselOptionDTO[];
 }
